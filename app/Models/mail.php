605c6a5b22a3d21411fe339b92d223305f003c72
Adfajare <alfian.ar256@gmail.com>
2025-09-03 18:27:02 +0700

    feat: category & mail schema & model

---
+++ app/Models/mail.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class mail extends Model
+{
+    protected $fillable = ['title', 'number', 'date', 'category_id', 'file_path'];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+}
